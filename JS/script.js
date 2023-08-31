ed7e1f7b9085d5c882296d8d2d8f64377db4a5fd
RishivinKannan <rishivinkannanrrk003@gmail.com>
2023-08-31 22:53:07 +0530

    added responsiveness in both project and techstack

--- JS/script.js
+++ JS/script.js
@@ -5,18 +5,27 @@ window.onload = function(){
     const menubtn= document.querySelector('.menubtn');
     const navlist= document.querySelector('.navlist');
     const closebtn= document.querySelector('.close');
+
+    const projectcard = document.getElementsByClassName('project-card');
+
     if(localStorage.getItem('Theme')){
         body.classList.add('dark-mode');
         mode_btn.classList.toggle('isdark');
-
         menubtn.classList.toggle('darkbtn');
-
+        for(i=0;i<projectcard.length;i++){
+            projectcard[i].classList.toggle('dark-card');
+        }
+        
     }
 
     mode_btn.onclick= function(){
         mode_btn.classList.toggle('isdark');
         menubtn.classList.toggle('darkbtn');
         body.classList.toggle('dark-mode');
+        for(i=0;i<projectcard.length;i++){
+            projectcard[i].classList.toggle('dark-card');
+        }
+
         if( body.classList.contains('dark-mode')){
             localStorage.setItem('Theme','dark-mode');
         }
